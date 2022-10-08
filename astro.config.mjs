3db897f085b3848318e1734695d1f4a2ba7275ca
CofCat456 <a25690756@gmail.com>
2022-10-08 15:08:00 +0800

    [update] eslint prettier auto fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import react from "@astrojs/react";
-import image from "@astrojs/image";
-import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
+import image from '@astrojs/image';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,                   // 一行最多200字元
+  tabWidth: 2,                       // Tab = 2個空白
+  useTabs: false,                    // 不使用縮進符，使用空格
+  semi: true,                        // 行尾需要有分號
+  singleQuote: true,                 // 使用單引號代替雙引號
+  trailingComma: 'none',             // 末尾使用逗號
+  jsxBracketSameLine: false,         // jsx Tag的尾端括號换行
+  bracketSpacing: true,              // 大括號內 首尾需要空白 { title: 'Opshell' }
+}
