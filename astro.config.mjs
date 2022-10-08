5668803c46c14fde86d8467f5569e5d6c33ffc2e
CofCat456 <a25690756@gmail.com>
2022-10-08 08:11:14 +0800

    [update] tailwind config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
-
 import image from "@astrojs/image";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), image()]
+  integrations: [mdx(), sitemap(), react(), image(), tailwind()]
 });
